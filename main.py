b8fae5bb221e4ef6fa0367ff6b764831a97a9b54
maryjane338 <glitch.tv.sav@gmail.com>
2024-10-19 13:21:35 +0900

    Check

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 class ConfigCache:
     __instance = None
 
-    def __new__(cls, *args, **kwargs):
+    def __new__(cls):
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)
         return cls.__instance
 
     def __init__(self):
-        if not hasattr(self, "_is_initialized"):
+        if not hasattr(self, "is_initialized"):
             self.instruction = None
             self.modules = None
             self.themes = None
-            self._is_initialized = True
+            self.is_initialized = True
 
     def make_cache(self, a, b, c):
         self.instruction = a
@@ -20,3 +20,12 @@ class ConfigCache:
 
     def show_cache(self):
         print(self.instruction, self.modules, self.themes, sep=' Kb, ', end=' Kb\n')
+
+
+cache1 = ConfigCache()
+cache1.make_cache(5, 60, 23)
+cache1.show_cache()
+
+cache2 = ConfigCache()
+cache2.show_cache()
+print(cache1 is cache2)
